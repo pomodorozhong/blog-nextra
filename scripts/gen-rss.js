20324dd0d65d0990d25d7dc1d6e189e935057079
pomodorozhong <pomodorozhong@gmail.com>
2022-04-05 17:03:33 +0800

    feat✨(gen-rss.js): update with my info

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'pomodorozhong\'s blog',
+    site_url: 'https://blog-pomodorozhong.vercel.app',
+    feed_url: 'https://blog-pomodorozhong.vercel.app/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
